5b2ae6cdbb47cfe20ca8525f2d4d08a5c5d7da1a
joostlek <joostlek@outlook.com>
2021-03-16 16:48:31 +0100

    Remove annotation

--- src/main/java/hu/indicium/battle/management/ManagementApplication.java
+++ src/main/java/hu/indicium/battle/management/ManagementApplication.java
@@ -2,10 +2,8 @@ package hu.indicium.battle.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
-@EnablePrometheusMetrics
 public class ManagementApplication {
 
     public static void main(String[] args) {
