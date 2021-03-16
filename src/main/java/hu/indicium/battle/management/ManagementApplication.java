3ec10c9d2a4217ee64a497753a504911f9862dea
joostlek <joostlek@outlook.com>
2021-03-16 16:38:55 +0100

    Add Prometheus

--- src/main/java/hu/indicium/battle/management/ManagementApplication.java
+++ src/main/java/hu/indicium/battle/management/ManagementApplication.java
@@ -2,8 +2,10 @@ package hu.indicium.battle.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
+@EnablePrometheusMetrics
 public class ManagementApplication {
 
     public static void main(String[] args) {
